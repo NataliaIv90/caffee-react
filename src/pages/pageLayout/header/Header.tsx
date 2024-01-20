b808244cd1fe20161f8a4a0a6e84abbcecf64681
Natalia Ivantsova <ivantsovana@gmail.com>
2024-01-20 20:37:00 +0200

    feat: add header and footer structure

--- src/pages/pageLayout/header/Header.tsx
+++ src/pages/pageLayout/header/Header.tsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import Logo from '../../../assets/images/logo.png';
+import CupIcon from '../../../assets/images/svg/cup.svg';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div>
+        <a href='./index.html'>
+          <img src={Logo} alt='Logo' />
+        </a>
+      </div>
+      <nav>
+        <ul>
+          <li>
+            <a href='#favourite-coffee'>
+              <span>Favorite coffee</span>
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <a href='#about'>
+              <span>About</span>
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <a href='#app-link'>
+              <span>Mobile app</span>
+              <span></span>
+            </a>
+          </li>
+          <li>
+            <a href='#contact-us'>
+              <span>Contact us</span>
+              <span></span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <a href='/'>
+          <span>
+            <span>Menu</span>
+            <img src={CupIcon} alt='Cup icon' />
+          </span>
+          <span></span>
+        </a>
+      </div>
+    </header >
   );
 };
